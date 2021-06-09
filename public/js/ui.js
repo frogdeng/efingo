63cc9ada56fa52cf1a0f9fe8adf23032efd39f76
鄧楨樺 <frogdeng0319@gmail.com>
2021-06-10 04:16:09 +0800

    0610

--- public/js/ui.js
+++ public/js/ui.js
@@ -15,15 +15,14 @@ $(document).ready(function(){
 
 		$(window).scroll(function (event) {
 
+		var sc_1 = $(window).scrollTop() ;
 
-      var sc_1 = $(window).scrollTop() ;
-
-      if(sc_1 > 200){
-        $(".index_menu").addClass('index_menu_bg')
-      }
-      else{
-        $(".index_menu").removeClass('index_menu_bg')
-      }
+		if(sc_1 > 200){
+			$(".index_menu").addClass('index_menu_bg')
+		}
+		else{
+			$(".index_menu").removeClass('index_menu_bg')
+		}
 
       // if(sc_1 > inside_slider_h){
       //   $(".inside_menu").addClass('index_menu_bg')
@@ -32,45 +31,9 @@ $(document).ready(function(){
       //   $(".inside_menu").removeClass('index_menu_bg')
       // }
 
-  });
+});
 
-
-
-
-
-
-
-
-
-
-
-	//   var bg_array = ["solution_bg_1", "solution_bg_2","solution_bg_3", "solution_bg_4"]
-
-
-	//   $('.des_show_1').click(function(){
-	//   	$('.solution_wrapper').removeClass(bg_array).addClass("solution_bg_1");
-	//   	$('.content_1').removeClass("gone").siblings().addClass("gone")
-
-	//   })
-
-	//   $('.des_show_2').click(function(){
-	//   	$('.solution_wrapper').removeClass(bg_array).addClass("solution_bg_2")
-	//   	$('.content_2').removeClass("gone").siblings().addClass("gone")
-	  	
-
-	//   })
-	// $('.des_show_3').click(function(){
-	//   	$('.solution_wrapper').removeClass(bg_array).addClass("solution_bg_3")
-	//   	$('.content_3').removeClass("gone").siblings().addClass("gone")
-
-	//   })
-	// $('.des_show_4').click(function(){
-	//   	$('.solution_wrapper').removeClass(bg_array).addClass("solution_bg_4")
-	//   	$('.content_4').removeClass("gone").siblings().addClass("gone")
-
-	//   })
 	
-
 
 });
 
